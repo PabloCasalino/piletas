fa804ab1ea7ed6b1cef3588709b5a7690f82b063
PabloCasalino <pabloangelcasalino@gmail.com>
2024-08-06 23:13:05 -0300

    trabajos en modelo

--- statics/scripts/formulario.js
+++ statics/scripts/formulario.js
@@ -2,22 +2,22 @@
         e.preventDefault();
 
         let nombre = document.getElementById('nombre').value;
-        // let apellido = document.getElementById('apellido').value;
         let telefono = document.getElementById('telefono').value;
         let barrio = document.getElementById('barrio').value;
         let fecha = document.getElementById('fecha').value;
         let vendedor = document.getElementById('vendedor').value;
         let email = document.getElementById('email').value;
+        let modelo = document.getElementById('modelo_select').value;
 
         localStorage.setItem('nombre',nombre);
-        // localStorage.setItem('apellido',apellido);
         localStorage.setItem('telefono',telefono);
         localStorage.setItem('barrio',barrio);
         localStorage.setItem('fecha',fecha);
         localStorage.setItem('vendedor',vendedor);
         localStorage.setItem('email',email);
+        localStorage.setItem('modelo_select',modelo);
         
-        window.location.href = 'presupuesto.html';
+         window.location.href = 'presupuesto.html';
 
     });
 
--- statics/scripts/presupuesto.js
+++ statics/scripts/presupuesto.js
@@ -1,12 +1,20 @@
 window.addEventListener('load', function() {
-
+    // Rellenar los campos del formulario
     document.getElementById("nombre").value = localStorage.getItem("nombre");
-    // document.getElementById("apellido").value =localStorage.getItem("apellido");
     document.getElementById('telefono').value = localStorage.getItem('telefono');
     document.getElementById('barrio').value = localStorage.getItem('barrio');
     document.getElementById('fecha').value = localStorage.getItem('fecha');
     document.getElementById('vendedor').value = localStorage.getItem('vendedor');
     document.getElementById('email').value = localStorage.getItem('email');
-    
-});
 
+ 
+    var modeloValor = localStorage.getItem('modelo_select');
+    
+
+    var modeloSpan = document.getElementById('modelo_presupuesto');
+    if (modeloSpan) {
+        modeloSpan.textContent = modeloValor;
+    } else {
+        console.error('Elemento con ID "modelo_presupuesto" no encontrado.');
+    }
+});
